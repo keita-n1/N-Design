133c5da260d659c34a414935cc9ea1f880d6ecfd
github keita-n1 <saku.nk@icloud.com>
2021-04-13 04:29:07 +0900

    ヘッダーのレスポンシブを追加

--- script/script.js
+++ script/script.js
@@ -4,6 +4,8 @@
 $(function() {
     $('.bl_hamburger').on('click', function() {
         $(this).toggleClass('active');
+        //ドロワー
+        $('.bl_headerNav_wrap').toggleClass('js_drawer');
         return false;
     });
 });
