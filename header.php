b5c4c1420c1feb7f5fbde478dfb0629b4d1cc50a
github keita-n1 <saku.nk@icloud.com>
2021-04-15 16:21:42 +0900

    nav h2 btn fontstyleを変更

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, minmum-scale=1">
         <!--google-font-->
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Rajdhani&display=swap" rel="stylesheet">
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
